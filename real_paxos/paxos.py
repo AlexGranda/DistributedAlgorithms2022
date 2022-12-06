0abe0e9a7df8fcee8ff064da0405cfcf24ca182d
DAVIDE CASNICI <258460@studenti.unimore.it>
2022-12-06 10:51:15 +0100

    quorums fix

--- real_paxos/paxos.py
+++ real_paxos/paxos.py
@@ -48,7 +48,7 @@ def acceptor(config, id):
         # if instance not in paxos_instances.keys():
         #    paxos_instances[instance] = init_state
         msg = None
-        msg = r.recv(2 ** 16)
+        msg = r.recv(2 ** 30)
         if msg is not None:
             decoded_message = Message(0, 'DECODING').decode(msg)
             message_instance = decoded_message.instance
@@ -56,7 +56,7 @@ def acceptor(config, id):
 
             if message_phase == '1A':
                 if message_instance not in state_dict.keys():
-                    #instance doesn't exists for the acceptor yet, we create a new state
+                    # instance doesn't exists for the acceptor yet, we create a new state
                     state_dict[message_instance] = {
                         'rnd': decoded_message.c_rnd,  # highest 1a c_rnd received
                         'v_rnd': 0,  # highest 2b c_rnd received
@@ -73,6 +73,8 @@ def acceptor(config, id):
                                                    v_val=state_dict[message_instance]['v_val'])
                         s.sendto(phase_1B_message.encode(), config['proposers'])
 
+                # print('acceptor', id, state_dict)
+
             elif message_phase == '2A':
                 if decoded_message.c_rnd >= state_dict[message_instance]['rnd']:
                     state_dict[message_instance]['v_rnd'] = decoded_message.c_rnd
@@ -88,10 +90,10 @@ def proposer(config, id):
     s = mcast_sender()
     state_dict = dict()
     quorum = math.ceil((ACCEPTORS + 1) / 2)
-
+    # print(id, quorum)
     while True:
         msg = None
-        msg = r.recv(2 ** 16)
+        msg = r.recv(2 ** 30)
 
         if msg is not None:
             client_message = True
@@ -105,15 +107,16 @@ def proposer(config, id):
                 # we know that the id is unique for each proposer with respect to the instance so we use it directly
                 # as c_rnd
                 state_dict[len(state_dict)] = {
-                    'quorum': 0,  # quorum counter
+                    'quorum1B': 0,  # quorum counter for phase 2A
+                    'quorum2B': 0, # quorum counter for DECISION
                     'value': msg.decode(),  # value to be proposed,
                     'phase': '1A',  # current proposer phase for this instance
                     'k': 1,
                     'k_v_val': None
                 }
-                phase_1A_message = Message(len(state_dict)-1, '1A', c_rnd=id)
+                phase_1A_message = Message(len(state_dict) - 1, '1A', c_rnd=id)
                 s.sendto(phase_1A_message.encode(), config['acceptors'])
-
+                #print('proposer', id, state_dict) # phase 1a working
             else:
                 decoded_message = Message(0, 'DECODING').decode(msg)
                 message_instance = decoded_message.instance
@@ -126,14 +129,15 @@ def proposer(config, id):
                 if state_dict[message_instance]['phase'] == '1A':
                     # we already know that the message phase is 1B
                     if id == message_rnd:
-                        state_dict[message_instance]['quorum'] += 1
+                        state_dict[message_instance]['quorum1B'] += 1
                         if state_dict[message_instance]['k'] < message_v_rnd:
                             state_dict[message_instance]['k'] = message_v_rnd
                             state_dict[message_instance]['k_v_val'] = message_v_val
                             # check quorum reached
-                    if state_dict[message_instance]['quorum'] == quorum:
-                        state_dict[message_instance]['quorum'] = -1
-                        value_to_be_proposed = state_dict[message_instance]['value'] if state_dict[message_instance]['k'] == 1 else \
+                    if state_dict[message_instance]['quorum1B'] == quorum:
+                        state_dict[message_instance]['quorum1B'] = -1
+                        value_to_be_proposed = state_dict[message_instance]['value'] if state_dict[message_instance][
+                                                                                            'k'] == 1 else \
                             state_dict[message_instance]['k_v_val']
                         state_dict[message_instance]['phase'] = '2A'
                         phase_2A_message = Message(message_instance, '2A', c_rnd=id,
@@ -143,22 +147,23 @@ def proposer(config, id):
                 elif state_dict[message_instance]['phase'] == '2A':
                     # if message is 1B we don't care anymore, since we already achieved the quorum for phase 1A for this instance
                     if message_phase == '2B' and message_v_rnd == id:  # we coount in the quorum only messages with v_rnd == c_rnd
-                        state_dict[message_instance]['quorum'] += 1
+                        state_dict[message_instance]['quorum2B'] += 1
                         # check quorum reached
-                        if state_dict[message_instance]['quorum'] == quorum:
-                            state_dict[message_instance]['quorum'] = -1
+                        if state_dict[message_instance]['quorum2B'] == quorum:
+                            state_dict[message_instance]['quorum2B'] = -1
                             value_to_be_proposed = message_v_val
                             state_dict[message_instance]['phase'] = 'DECISION'
                             phase_DECISION_message = Message(message_instance, 'DECISION', v_val=value_to_be_proposed)
                             s.sendto(phase_DECISION_message.encode(),
-                                     config['learners'])  # TODO check how to handle this for the prder
+                                     config['learners'])
+                            #print('proposer', id, state_dict)
 
 
 def learner(config, id):
     r = mcast_receiver(config['learners'])
     while True:
         msg = None
-        msg = r.recv(2 ** 16)
+        msg = r.recv(2 ** 30)
         if msg is not None:
             msg = Message(0, 'DECODING').decode(msg)  # create instance of Message for the received decision
             msg = msg.v_val  # retrieve only the value
